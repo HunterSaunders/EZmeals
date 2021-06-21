e683487773d987a7bbfa5b4e385b259b0dba41c2
Hailey <gri06001@byui.edu>
2021-06-21 13:08:44 -0700

    commit on MainActivity variables to build url to search API

--- app/src/main/java/com/example/ezmeals/MainActivity.java
+++ app/src/main/java/com/example/ezmeals/MainActivity.java
@@ -7,9 +7,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import com.google.gson.Gson;
+
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private static int SPLASH_SCREEN_TIME_OUT = 4000;
+    private static final String TAG = "MainActivity";
+//    private final String key = getString(R.string.api_key); //api key from resource file: res/values/secrets.xml
+//    private final String appId = getString(R.string.app_id); //app id from resource file: res/values/secrets.xml
+//    private final String searchTerms = "chicken%20soup"; //How do you add multiple search terms? Add them to a list? This is here for testing purposes
+//    private final String baseUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=";
+//    private final String url = baseUrl + searchTerms + "&app_id=" + appId + "&app_key=" + key;
+    private Gson gson;
+    private HTTPHelper httpHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,4 +38,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }, SPLASH_SCREEN_TIME_OUT);
     }
-}
+
+
+    }
