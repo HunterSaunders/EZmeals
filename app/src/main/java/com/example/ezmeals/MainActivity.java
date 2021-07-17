60d729f3c9a9654df1224decafd271ff9ade0a03
gamezmasterz <ber20024@byui.edu>
2021-07-17 10:09:41 -0700

    Re-added comments to MainActivity.java (as they disappeared).

--- app/src/main/java/com/example/ezmeals/MainActivity.java
+++ app/src/main/java/com/example/ezmeals/MainActivity.java
@@ -8,10 +8,10 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    //display load screen with a timeout
     private static int SPLASH_SCREEN_TIME_OUT = 4000;
 
-
+    //create and initialize the main activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 
         new Handler().postDelayed(new Runnable(){
+            //start Home.class to transition the display and functionality to the Home activity
             @Override
             public void run(){
                 Intent homeIntent = new Intent(MainActivity.this, Home.class);
