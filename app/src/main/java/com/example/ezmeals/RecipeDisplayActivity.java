39dff2b7a16e1ee2d8d6dcb7421e1effbb073aaf
SteverMemer <ogd19006@byui.edu>
2021-07-17 10:46:06 -0600

    added my comments

--- app/src/main/java/com/example/ezmeals/RecipeDisplayActivity.java
+++ app/src/main/java/com/example/ezmeals/RecipeDisplayActivity.java
@@ -43,33 +43,47 @@ import static com.example.ezmeals.BuildConfig.api_key;
 
 public class RecipeDisplayActivity extends AppCompatActivity {
 
+    //JSON objects of recipe info to be used throughout
     JSONObject recipeData;
     JSONArray recipeIngredients;
 
+    //private variables for the api key and id so that the app is able to us the api
     private final String key = api_key;
     private final String appId = api_id;
+
     private Button alertButton;
     private TextView alertTextView;
 
     @Override
+    //load the saved previous state of the application
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //display the basic layout of the recipe screen
         setContentView(R.layout.recipe_screen);
+
+        //add a title to the page
         getSupportActionBar().setTitle("Recipe");
 
+        //list view of the ingredients to make each ingredient able to separate
         ListView lv = (ListView) findViewById(R.id.ingredientListView);
         List<String> ingredientList = new ArrayList<String>();
 
         Intent intent = getIntent();
+        //original recipe link from api data
         String recipeLink = intent.getStringExtra(SearchAPI.SRC_LINK);
 
+        //a few basic variables put into specific spots on the recipe screen
         TextView recipeName = (TextView) findViewById(R.id.recipe_name);
         TextView cuisineType = (TextView) findViewById(R.id.textView5);
         TextView dishType = (TextView) findViewById(R.id.textView8);
         TextView meal = (TextView) findViewById(R.id.textView10);
         ImageView recipeImage = (ImageView) findViewById(R.id.recipe_screen_img);
+
+        //button for the original recipe to open into google chrome
         Button urlBtn = (Button) findViewById(R.id.instructions_btn);
 
+        //here is the link to the api where all of the information on a recipe is sourced
         RequestQueue queue = Volley.newRequestQueue(RecipeDisplayActivity.this);
         String url = "https://api.edamam.com/api/recipes/v2/" + recipeLink + "?type=public&app_id=" + appId + "&app_key=" + key;
 
@@ -82,26 +96,32 @@ public class RecipeDisplayActivity extends AppCompatActivity {
                     recipeIngredients = recipeData.getJSONArray("ingredientLines");
                     JSONArray recipeIngredients = recipeData.getJSONArray("ingredientLines");
 
+                    //loop to add each ingredient one by on into a list
                     if (recipeIngredients != null) {
                         int len = recipeIngredients.length();
 
                         for (int i=0;i<len;i++){
                             ingredientList.add(recipeIngredients.get(i).toString());
-                            //ingredientsListItems.add(new GroceryItem(recipeIngredients.get(i).toString()));
                         }
                     }
 
                     RequestOptions requestOptions=new RequestOptions();
                     requestOptions.placeholder(R.drawable.ic_ez_logo);
                     requestOptions.error(R.drawable.ic_ez_logo);
+
+                    //add the recipes image onto the recipe screen
                     Glide.with(RecipeDisplayActivity.this)
                             .load(recipeData.getString("image"))
                             .apply(requestOptions)
                             .into(recipeImage);
+
+                    //Here are the specific categories that will show up on the recipe screen
                     recipeName.setText(recipeData.getString("label"));
                     cuisineType.setText(recipeData.getString("cuisineType").replace("/","").replace("[","").replace("]","").replace("\\"," or ").replace("\"",""));
                     dishType.setText(recipeData.getString("dishType").replace("/","").replace("[","").replace("]","").replace("\\"," or ").replace("\"",""));
                     meal.setText(recipeData.getString("mealType").replace("/","").replace("[","").replace("]","").replace("\\","/").replace("\"",""));
+
+                    //little function to open the recipe link
                     urlBtn.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View v) {
@@ -119,6 +139,7 @@ public class RecipeDisplayActivity extends AppCompatActivity {
                     ArrayAdapter arrayAdapter = new ArrayAdapter(RecipeDisplayActivity.this, android.R.layout.simple_list_item_1, ingredientList);
                     lv.setAdapter(arrayAdapter);
 
+                    //function to add specific ingredients when clicked to the grocery list
                     lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                         @Override
                         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -152,12 +173,14 @@ public class RecipeDisplayActivity extends AppCompatActivity {
                             editor.putString("Grocery List", jsonItems);
                             editor.apply();
 
+                            //notification to say that you have added an ingredient to the list
                             AlertDialog.Builder builder = new AlertDialog.Builder(RecipeDisplayActivity.this);
 
                             builder.setCancelable(true);
                             builder.setTitle("EZmeals Alert");
                             builder.setMessage("Ingredients saved");
 
+                            //cancel added ingredient
                             builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialogInterface, int i) {
@@ -165,6 +188,7 @@ public class RecipeDisplayActivity extends AppCompatActivity {
                                 }
                             });
 
+                            //view added ingredient on list
                             builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
                                 @Override
                                 public void onClick(DialogInterface dialog, int i) {
@@ -191,6 +215,7 @@ public class RecipeDisplayActivity extends AppCompatActivity {
 
 
 
+        //basic layout for the bottom navigation view and on click to take you to each part
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setSelectedItemId(android.R.id.home);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
